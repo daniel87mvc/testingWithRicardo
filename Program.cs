3dc16899127bc2ebd37d5a4e05cbbc5e8b3dabdb
Ricardo Miranda <ricardomiranda0320@gmail.com>
2018-01-31 16:31:48 -0500

    Use '--' to separate paths from revisions, like this:
    'git <command> [<revision>...] -- [<file>...]'

---
+++ Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace testingWithRicardo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            Console.WriteLine("Write your Name!");
+            Console.WriteLine("Write your Age!");
+            Console.Read();
+
+        }
+    }
+}
